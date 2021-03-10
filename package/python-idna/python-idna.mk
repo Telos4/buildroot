50990f3db4c3968ad4f65e7d683af6e14d2a09cc
Simon Pirkelmann <simon.pirkelmann@gmail.com>
2021-03-10 08:54:52 +0100

    replaced manuallaly generated package configuration by automatically generated config using buildroot's utils/scanpypi Flask-Security-Too

--- package/python-idna/python-idna.mk
+++ package/python-idna/python-idna.mk
@@ -4,11 +4,10 @@
 #
 ################################################################################
 
-PYTHON_IDNA_VERSION = 2.10
+PYTHON_IDNA_VERSION = 3.1
 PYTHON_IDNA_SOURCE = idna-$(PYTHON_IDNA_VERSION).tar.gz
-PYTHON_IDNA_SITE = https://files.pythonhosted.org/packages/ea/b7/e0e3c1c467636186c39925827be42f16fee389dc404ac29e930e9136be70
-PYTHON_IDNA_LICENSE = BSD-3-Clause
-PYTHON_IDNA_LICENSE_FILES = LICENSE.rst
+PYTHON_IDNA_SITE = https://files.pythonhosted.org/packages/9f/24/1444ee2c9aee531783c031072a273182109c6800320868ab87675d147a05
 PYTHON_IDNA_SETUP_TYPE = setuptools
+PYTHON_IDNA_LICENSE = FIXME: please specify the exact BSD version
 
 $(eval $(python-package))
