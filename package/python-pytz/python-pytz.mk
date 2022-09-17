0d6d72442b4019aee3e6a779336460b4c8b69d9b
Simon Pirkelmann <simon.pirkelmann@gmail.com>
2022-09-17 18:26:32 +0200

    updated version of python-pytz and added it to host system s.t. it's available at build time

--- package/python-pytz/python-pytz.mk
+++ package/python-pytz/python-pytz.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-PYTHON_PYTZ_VERSION = 2019.3
+PYTHON_PYTZ_VERSION = 2022.2.1
 PYTHON_PYTZ_SOURCE = pytz-$(PYTHON_PYTZ_VERSION).tar.gz
-PYTHON_PYTZ_SITE = https://files.pythonhosted.org/packages/82/c3/534ddba230bd4fbbd3b7a3d35f3341d014cca213f369a9940925e7e5f691
+PYTHON_PYTZ_SITE = https://files.pythonhosted.org/packages/24/0c/401283bb1499768e33ddd2e1a35817c775405c1f047a9dc088a29ce2ea5d
 PYTHON_PYTZ_SETUP_TYPE = setuptools
 PYTHON_PYTZ_LICENSE = MIT
 PYTHON_PYTZ_LICENSE_FILES = LICENSE.txt
 
 $(eval $(python-package))
+$(eval $(host-python-package))
