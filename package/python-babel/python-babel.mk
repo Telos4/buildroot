935baaed509de45deb3046b328e0a46d9533fb6d
Simon Pirkelmann <simon.pirkelmann@gmail.com>
2022-09-17 18:26:01 +0200

    updated version of python-babel and added it to host system s.t. it's available at build time

--- package/python-babel/python-babel.mk
+++ package/python-babel/python-babel.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-PYTHON_BABEL_VERSION = 2.7.0
+PYTHON_BABEL_VERSION = 2.10.3
 PYTHON_BABEL_SOURCE = Babel-$(PYTHON_BABEL_VERSION).tar.gz
-PYTHON_BABEL_SITE = https://files.pythonhosted.org/packages/bd/78/9fb975cbb3f4b136de2cd4b5e5ce4a3341169ebf4c6c03630996d05428f1
+PYTHON_BABEL_SITE = https://files.pythonhosted.org/packages/51/27/81e9cf804a34a550a47cc2f0f57fe4935281d479ae3a0ac093d69476f221
 PYTHON_BABEL_SETUP_TYPE = setuptools
-PYTHON_BABEL_LICENSE = BSD-3-Clause
-PYTHON_BABEL_LICENSE_FILES = LICENSE
+PYTHON_BABEL_LICENSE = FIXME: please specify the exact BSD version
+PYTHON_BABEL_LICENSE_FILES = LICENSE docs/license.rst docs/_themes/LICENSE
 
 $(eval $(python-package))
+$(eval $(host-python-package))
