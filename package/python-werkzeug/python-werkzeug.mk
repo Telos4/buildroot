e0cab68e2bd9f04092b4b2734b2133589841d859
Simon Pirkelmann <simon.pirkelmann@gmail.com>
2022-09-17 17:55:02 +0200

    updated version of python-werkzeug and added it to host system s.t. it's available at build time

--- package/python-werkzeug/python-werkzeug.mk
+++ package/python-werkzeug/python-werkzeug.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-PYTHON_WERKZEUG_VERSION = 1.0.1
+PYTHON_WERKZEUG_VERSION = 2.2.2
 PYTHON_WERKZEUG_SOURCE = Werkzeug-$(PYTHON_WERKZEUG_VERSION).tar.gz
-PYTHON_WERKZEUG_SITE = https://files.pythonhosted.org/packages/10/27/a33329150147594eff0ea4c33c2036c0eadd933141055be0ff911f7f8d04
+PYTHON_WERKZEUG_SITE = https://files.pythonhosted.org/packages/f8/c1/1c8e539f040acd80f844c69a5ef8e2fccdf8b442dabb969e497b55d544e1
 PYTHON_WERKZEUG_SETUP_TYPE = setuptools
 PYTHON_WERKZEUG_LICENSE = FIXME: please specify the exact BSD version
-PYTHON_WERKZEUG_LICENSE_FILES = LICENSE.rst
+PYTHON_WERKZEUG_LICENSE_FILES = LICENSE.rst docs/license.rst
 
 $(eval $(python-package))
+$(eval $(host-python-package))
