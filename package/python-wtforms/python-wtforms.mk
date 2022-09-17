2f959046bf5687275c8b2546702c02bf4404bcd5
Simon Pirkelmann <simon.pirkelmann@gmail.com>
2022-09-17 18:18:59 +0200

    updated python-wtforms

--- package/python-wtforms/python-wtforms.mk
+++ package/python-wtforms/python-wtforms.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_WTFORMS_VERSION = 2.3.3
+PYTHON_WTFORMS_VERSION = 3.0.1
 PYTHON_WTFORMS_SOURCE = WTForms-$(PYTHON_WTFORMS_VERSION).tar.gz
-PYTHON_WTFORMS_SITE = https://files.pythonhosted.org/packages/dd/3f/f25d26b1c66896e2876124a12cd8be8f606abf4e1890a20f3ca04e4a1555
+PYTHON_WTFORMS_SITE = https://files.pythonhosted.org/packages/9a/7d/d4aa68f5bfcb91dd61a7faf0e862512ae7b3d531c41f24c217910aec0559
 PYTHON_WTFORMS_SETUP_TYPE = setuptools
 PYTHON_WTFORMS_LICENSE = FIXME: please specify the exact BSD version
 PYTHON_WTFORMS_LICENSE_FILES = LICENSE.rst docs/license.rst
