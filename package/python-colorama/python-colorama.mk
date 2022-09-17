10c691866d8cd4ad36e83740111f0a32aaac0c41
Simon Pirkelmann <simon.pirkelmann@gmail.com>
2022-09-17 18:17:05 +0200

    updated python-colorama

--- package/python-colorama/python-colorama.mk
+++ package/python-colorama/python-colorama.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-PYTHON_COLORAMA_VERSION = 0.4.4
+PYTHON_COLORAMA_VERSION = 0.4.5
 PYTHON_COLORAMA_SOURCE = colorama-$(PYTHON_COLORAMA_VERSION).tar.gz
-PYTHON_COLORAMA_SITE = https://files.pythonhosted.org/packages/1f/bb/5d3246097ab77fa083a61bd8d3d527b7ae063c7d8e8671b1cf8c4ec10cbe
+PYTHON_COLORAMA_SITE = https://files.pythonhosted.org/packages/2b/65/24d033a9325ce42ccbfa3ca2d0866c7e89cc68e5b9d92ecaba9feef631df
 PYTHON_COLORAMA_SETUP_TYPE = setuptools
-PYTHON_COLORAMA_LICENSE = BSD-3-Clause
+PYTHON_COLORAMA_LICENSE = FIXME: please specify the exact BSD version
 PYTHON_COLORAMA_LICENSE_FILES = LICENSE.txt
 
 $(eval $(python-package))
