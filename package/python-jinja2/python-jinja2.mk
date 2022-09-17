a254f87c8e9118668b13c76372a2ec787ac23621
Simon Pirkelmann <simon.pirkelmann@gmail.com>
2022-09-17 17:55:46 +0200

    updated version of python-jinja2 and added it to host system s.t. it's available at build time

--- package/python-jinja2/python-jinja2.mk
+++ package/python-jinja2/python-jinja2.mk
@@ -4,14 +4,12 @@
 #
 ################################################################################
 
-# Please keep in sync with package/python3-jinja2/python3-jinja2.mk
-PYTHON_JINJA2_VERSION = 2.11.3
+PYTHON_JINJA2_VERSION = 3.1.2
 PYTHON_JINJA2_SOURCE = Jinja2-$(PYTHON_JINJA2_VERSION).tar.gz
-PYTHON_JINJA2_SITE = https://files.pythonhosted.org/packages/4f/e7/65300e6b32e69768ded990494809106f87da1d436418d5f1367ed3966fd7
+PYTHON_JINJA2_SITE = https://files.pythonhosted.org/packages/7a/ff/75c28576a1d900e87eb6335b063fab47a8ef3c8b4d88524c4bf78f670cce
 PYTHON_JINJA2_SETUP_TYPE = setuptools
 PYTHON_JINJA2_LICENSE = FIXME: please specify the exact BSD version
-PYTHON_JINJA2_LICENSE_FILES = LICENSE.rst
-PYTHON_JINJA2_CPE_ID_VENDOR = pocoo
-PYTHON_JINJA2_CPE_ID_PRODUCT = jinja2
+PYTHON_JINJA2_LICENSE_FILES = LICENSE.rst docs/license.rst
 
 $(eval $(python-package))
+$(eval $(host-python-package))
